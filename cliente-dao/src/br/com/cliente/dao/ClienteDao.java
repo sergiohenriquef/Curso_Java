0b322ab3f006d35d99058db345bf1162cfe90409
Jan <Jan@192.168.5.129>
2017-11-21 18:46:29 -0200

    Adicionando string de conexao.

--- cliente-dao/src/br/com/cliente/dao/ClienteDao.java
+++ cliente-dao/src/br/com/cliente/dao/ClienteDao.java
@@ -7,6 +7,10 @@ import br.com.cliente.model.Cliente;
 
 public class ClienteDao implements IDefaultDao {
 	
+	//vmdbsjan.database.windows.net
+	//jan
+	//jnasci@01
+	
 	private static String urlConnection = "jdbc:sqlserver://vmdbsjan.database.windows.net:1433;"
 			+ "database=db_cursos;"
 			+ "user=jan@vmdbsjan;"
